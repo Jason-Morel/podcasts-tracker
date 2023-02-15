11f042b8ea1e7efbbd35795cf090cae61a9859de
Jason Morel <120559557+LaDjazzz@users.noreply.github.com>
2023-02-15 16:03:15 +0100

    Authentification

---
+++ Authentification_2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 15 15:45:28 2023
+
+@author: Jason
+"""
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+#Authentication - without user
+client_credentials_manager = SpotifyClientCredentials(client_id="e48f42372a074a25b7a0d25da48439d6", client_secret="90eb460ff94847998926f6d380532f59")
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f"
+playlist_URI = playlist_link.split("/")[-1].split("?")[0]
+track_uris = [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
