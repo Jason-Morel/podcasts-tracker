61dd6b0033bb4c6ab16b209cec0f650486d8a420
lynabenyahia <122797987+lynabenyahia@users.noreply.github.com>
2023-02-20 12:14:40 +0100

    TELEGRAM_start

---
+++ TELEGRAM_start.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 20 12:12:01 2023
+
+@author: lyna
+"""
+
+token = "6179108053:AAFXqqyrlrLvN_tlSARu2_l3TLXkA_EjXTc" # donné par le compte BotFather quand on lui envoie /newbot et qu'on crée notre bot
+
+chat_id = 5561504638  # envoyer un message au bot avec son compte puis récupérer son id sur : https://api.telegram.org/bot6179108053:AAFXqqyrlrLvN_tlSARu2_l3TLXkA_EjXTc/getUpdates (ce qui est entre /bot et /getUpdates correspond au token)
