f68ec855782c4eb9d983d22ef22aea59bb0e2a28
lynabenyahia <122797987+lynabenyahia@users.noreply.github.com>
2023-02-20 12:39:33 +0100

    Update TELEGRAM_start.py

--- TELEGRAM_start.py
+++ TELEGRAM_start.py
@@ -5,7 +5,14 @@ Created on Mon Feb 20 12:12:01 2023
 
 @author: lyna
 """
+# pip install telegram_send
+# écrire dans la console telegram-send --configure
 
 token = "6179108053:AAFXqqyrlrLvN_tlSARu2_l3TLXkA_EjXTc" # donné par le compte BotFather quand on lui envoie /newbot et qu'on crée notre bot
 
 chat_id = 5561504638  # envoyer un message au bot avec son compte puis récupérer son id sur : https://api.telegram.org/bot6179108053:AAFXqqyrlrLvN_tlSARu2_l3TLXkA_EjXTc/getUpdates (ce qui est entre /bot et /getUpdates correspond au token)
+
+
+# Remplacer chaque print par messsages = "..."
+# Mettre à la fin du code : telegram_send.send(messages=[messages])
+
