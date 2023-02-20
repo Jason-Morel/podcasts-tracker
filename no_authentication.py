0f352243c19df6357d491e331ee5cd1bda671321
Jason Morel <jason.morel3813@gmail.com>
2023-02-20 16:16:09 +0100

    Finding shows using their ID

--- no_authentication.py
+++ no_authentication.py
@@ -9,6 +9,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 #Authentication - without user
+##We need to figure out how to secure the authentication! (keep id and secret in a different file and add a .gitignore)
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="e48f42372a074a25b7a0d25da48439d6",
                                                            client_secret="90eb460ff94847998926f6d380532f59"))
 
@@ -17,17 +18,24 @@ results = sp.search(q='weezer', limit=20)
 for idx, track in enumerate(results['tracks']['items']):
     print(idx, track['name'])
 
-#Try to find 10 shows related to economy in French market
+#Find 10 shows related to economy in French market
 shows = sp.search(q='économie', limit=10, type='show', market='FR')
 for idx, show in enumerate(shows['shows']['items']):
     print(show['publisher'], ':', show['name'])
     
-#Try to find 10 shows related to history in French market
+#Find 10 shows related to history in French market
 shows_history = sp.search(q='histoire', limit=10, type='show', market='FR')
 for idx, show in enumerate(shows_history['shows']['items']):
     print(show['publisher'], ':', show['name'], show['id'])
 
-#Try to find 10 episodes related to art in French market
+#Find 10 episodes related to art in French market
 shows_art = sp.search(q='art', limit=10, type='episode', market='FR')
 for idx, episode in enumerate(shows_art['episodes']['items']):
     print(episode['name'], episode['id'])
+    
+#Find a show using it's ID
+shows_id = sp.show(show_id='2ZFDmgDS2Z6xccP51s1zFQ', market='FR')
+
+#Find episodes using show ID
+episode = sp.show_episodes(show_id='2ZFDmgDS2Z6xccP51s1zFQ', limit=50, market='FR')
+
