79af07a48afa5e13fabd1bf53e7437948d62b1cc
lynabenyahia <122797987+lynabenyahia@users.noreply.github.com>
2023-03-01 14:11:18 +0100

    Update TELEGRAM_version.py

--- TELEGRAM_version.py
+++ TELEGRAM_version.py
@@ -12,31 +12,26 @@ import spotipy # la librairie pour manipuler l'api spotify
 import spotipy.util as util
 import requests
 import re
-from secret import *
 
-# Infos du telegram podcast_tracker_bot :
-TOKEN_telegram1 = TOKEN_telegram1
-chat_id = chat_id
-    
-# Infos du telegram show_finder_bot :
-TOKEN_telegram2 = TOKEN_telegram2
+# Infos de mon telegram :
+TOKEN_telegram = "6179108053:AAFXqqyrlrLvN_tlSARu2_l3TLXkA_EjXTc" # obtenu en créant notre bot avec le telegram BotFather
+chat_id = "5561504638" #obtenu en allant sur https://api.telegram.org/bot{TOKEN_telegram}/getUpdates
 
-# Authentification spotipy
-username = username
-clientId = clientId
-clientSecret = clientSecret
-scope = scope
-
+# AUTHENTIFICATION spotipy
+username="31aon4o2j7wikppjnfxfvpvptjtu?si=442993df78a74794"
+clientId= "e48f42372a074a25b7a0d25da48439d6"
+clientSecret="90eb460ff94847998926f6d380532f59"
+ 
+scope = 'playlist-modify-public'
 token = util.prompt_for_user_token(username,scope,client_id=clientId,client_secret=clientSecret,redirect_uri='https://github.com/Jason-Morel/podcasts-tracker')
-
 if token:
      sp = spotipy.Spotify(auth=token)
      sp.trace = False
-
+     
 
 # fonction pour envoyer un message à Telegram
 def send_telegram_message(message): # demander à Jason pour fonction propre
-    url = f"https://api.telegram.org/bot{TOKEN_telegram1}/sendMessage?chat_id={chat_id}&text={message}"
+    url = f"https://api.telegram.org/bot{TOKEN_telegram}/sendMessage?chat_id={chat_id}&text={message}"
     response = requests.get(url)
     
 
@@ -51,7 +46,7 @@ send_telegram_message("Entrez le numéro correspondant à votre choix : ")
 # RE RUN À PARTIR D'ICI
 
 # récupérer la réponse de l'utilisateur
-response = requests.get(f"https://api.telegram.org/bot{TOKEN_telegram1}/getUpdates") 
+response = requests.get(f"https://api.telegram.org/bot{TOKEN_telegram}/getUpdates") 
 data = response.json()
 result = data["result"][-1]
 text = result["message"]["text"]
@@ -86,7 +81,7 @@ elif time_choice == 7:
 send_telegram_message("Quel type de podcast souhaitez-vous écouter aujourd'hui ?\nEntrez le thème de votre choix : ")
    
 # Récupérer le mot exact entré sur Telegram
-response = requests.get(f"https://api.telegram.org/bot{TOKEN_telegram1}/getUpdates")
+response = requests.get(f"https://api.telegram.org/bot{TOKEN_telegram}/getUpdates")
 data = response.json()
 result = data["result"][-1]
 text = result["message"]["text"]
