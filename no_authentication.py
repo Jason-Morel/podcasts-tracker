c22ca23d8e6099d3b9efab3974564636d9f02730
Jason Morel <jason.morel3813@gmail.com>
2023-03-03 18:45:13 +0100

    Transform code into functions

--- no_authentication.py
+++ no_authentication.py
@@ -8,18 +8,25 @@ Created on Wed Feb 15 15:45:28 2023
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 import pandas as pd
 import re
 from colorama import Fore, Back, Style
 
+#Get client_id and client_secret from a .env file. 
+#We do this because it is unsafe to show one's API id/secret/token online as anyone could use them at our cost (if we pay to use the API).
+#SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
+#SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
+#Can't authenticate when id and secret are stored in .env file -> we need to figure out why.
 
 
+#client_credentials_manager = SpotifyClientCredentials(client_id=os.getenv('SPOTIPY_CLIENT_ID'), client_secret=os.getenv('SPOTIPY_CLIENT_SECRET'))
+#sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 #Authentication - without user
 def authenticate():
-    CLIENT_ID = os.environ.get('CLIENT_ID')
-    CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
-    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="CLIENT_ID",
-                                                               client_secret="CLIENT_SECRET"))
+    sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id='e48f42372a074a25b7a0d25da48439d6',
+                                                               client_secret='90eb460ff94847998926f6d380532f59'))
+                                                               
     return sp
 
 sp = authenticate()
@@ -31,16 +38,20 @@ input_language = 'fr'
 input_key_words = 'arbre'
 input_duration =  '5 to 15'
 
+#Algo input
+start_offset = 0
+
 
 
 #Find shows related to economy in French market
-def get_shows(key_words, language):
-    shows = sp.search(q=key_words, limit=50, type='show', market=language)
+def get_shows(key_words, language, input_offset):
+    shows = sp.search(q=key_words, limit=50, offset=input_offset, type='show', market=language)
     return shows
     #for idx, show in enumerate(shows['shows']['items']):
         #print(show['publisher'], ':', show['name'], show['languages'], show['id'])
 
-shows = get_shows(key_words=input_key_words, language=input_language)
+shows = get_shows(key_words=input_key_words, language=input_language, input_offset=start_offset)
+start_offset += 50
 
 
 
@@ -75,83 +86,128 @@ shows = remove_other_languages(language=input_language)
 
 ##Look at the distribution of the variable duration_ms for the set of episodes
 #1-Add a new key (called 'episodes') to the shows['shows']['items'][idx] dictionaries. It's a dictionary which contains descriptions of the show's first 50 episodes.
-for idx, show in enumerate(shows['shows']['items']):
-    shows['shows']['items'][idx]['episodes'] = sp.show_episodes(show_id=shows['shows']['items'][idx]['id'], limit=50, market='FR')
+def get_episodes(language):
+    for idx, show in enumerate(shows['shows']['items']):
+        shows['shows']['items'][idx]['episodes'] = sp.show_episodes(show_id=shows['shows']['items'][idx]['id'], limit=50, market=language)
+    return shows
+        
+shows = get_episodes(language=input_language)
 
 #2-Add a new key (called 'duration_ms') to the shows['shows']['items'][idx] dictionaries. It's a list which contains durations (in ms) of the show's first 50 episodes. 
-for idx, show in enumerate(shows['shows']['items']):
-    duration_ms = []
-    for i, episode in enumerate(shows['shows']['items'][idx]['episodes']['items']):
-        duration_ms.append(shows['shows']['items'][idx]['episodes']['items'][i]['duration_ms'])
-        shows['shows']['items'][idx]['durations_ms'] = duration_ms
+def get_durations():
+    for idx, show in enumerate(shows['shows']['items']):
+        duration_ms = []
+        for i, episode in enumerate(shows['shows']['items'][idx]['episodes']['items']):
+            duration_ms.append(shows['shows']['items'][idx]['episodes']['items'][i]['duration_ms'])
+            shows['shows']['items'][idx]['durations_ms'] = duration_ms
+    return shows
+
+shows = get_durations()
     
 #3-Find min and max values of the duration_ms list
-#3.1-Convert lists into Pandas DataFrames
-for idx, show in enumerate(shows['shows']['items']):
-    shows['shows']['items'][idx]['durations_ms'] = pd.DataFrame({'duration_ms':shows['shows']['items'][idx]['durations_ms']})
-#Add a new key (called 'min_max') to the shows['shows']['items'][idx] dictionaries. It's a dictionary which contains the min (10th percentile) and max (90th percentile) of the show's first 50 episodes. 
-for idx, show in enumerate(shows['shows']['items']):
-    durations_ms = shows['shows']['items'][idx]['durations_ms']
-    shows['shows']['items'][idx]['min_max'] = {'min':round(int(durations_ms.quantile(q=0.1))/60000,2), 'max':round(int(durations_ms.quantile(q=0.9))/60000,2)} #min and max are converted to minutes
+def get_min_max():
+    #3.1-Convert lists into Pandas DataFrames
+    for idx, show in enumerate(shows['shows']['items']):
+        shows['shows']['items'][idx]['durations_ms'] = pd.DataFrame({'duration_ms':shows['shows']['items'][idx]['durations_ms']})
+
+    #3.2-Add a new key (called 'min_max') to the shows['shows']['items'][idx] dictionaries. It's a dictionary which contains the min (10th percentile) and max (90th percentile) of the show's first 50 episodes. 
+    for idx, show in enumerate(shows['shows']['items']):
+        durations_ms = shows['shows']['items'][idx]['durations_ms']
+        shows['shows']['items'][idx]['min_max'] = {'min':round(int(durations_ms.quantile(q=0.1))/60000,2), 'max':round(int(durations_ms.quantile(q=0.9))/60000,2)} #min and max are converted to minutes
+    return shows
+
+shows = get_min_max()
 
 #To have an idea of the intervals we can expect with this method:
 for idx, show in enumerate(shows['shows']['items']):
     print(shows['shows']['items'][idx]['min_max'])
     
-#Drop shows for which episode duration is not regular enough    
-for idx, show in enumerate(shows['shows']['items']):
-    if shows['shows']['items'][idx]['min_max']['min']<45:
-        if shows['shows']['items'][idx]['min_max']['max']-shows['shows']['items'][idx]['min_max']['min']>15:
-            #print(shows['shows']['items'][idx]['min_max'])
-            del shows['shows']['items'][idx]
+#Drop shows for which episode duration is not regular enough
+def keep_shows_with_regular_duration():  
+    for idx, show in enumerate(shows['shows']['items']):
+        if shows['shows']['items'][idx]['min_max']['min']<45:
+            if shows['shows']['items'][idx]['min_max']['max']-shows['shows']['items'][idx]['min_max']['min']>15:
+                #print(shows['shows']['items'][idx]['min_max'])
+                del shows['shows']['items'][idx]
+    return shows
+
+shows = keep_shows_with_regular_duration()
 
 ##Assign a duration span to each show
 #Round min and max to get clean duration spans
-for idx, show in enumerate(shows['shows']['items']):
-    if shows['shows']['items'][idx]['min_max']['min']>5:
-        shows['shows']['items'][idx]['min_max']['min'] = int(round(shows['shows']['items'][idx]['min_max']['min'] - shows['shows']['items'][idx]['min_max']['min']%5, 0))
-        shows['shows']['items'][idx]['min_max']['max'] = int(round(shows['shows']['items'][idx]['min_max']['max'] - (shows['shows']['items'][idx]['min_max']['max']%5), 0))
+def round_min_max():
+    for idx, show in enumerate(shows['shows']['items']):
+        if shows['shows']['items'][idx]['min_max']['min']>5:
+            shows['shows']['items'][idx]['min_max']['min'] = int(round(shows['shows']['items'][idx]['min_max']['min'] - shows['shows']['items'][idx]['min_max']['min']%5, 0))
+            shows['shows']['items'][idx]['min_max']['max'] = int(round(shows['shows']['items'][idx]['min_max']['max'] - (shows['shows']['items'][idx]['min_max']['max']%5), 0))
+    
+    for idx, show in enumerate(shows['shows']['items']):
+        if shows['shows']['items'][idx]['min_max']['min']<5 and shows['shows']['items'][idx]['min_max']['max']<6:
+            shows['shows']['items'][idx]['min_max']['min'] = 0
+            shows['shows']['items'][idx]['min_max']['max'] = 5
+    
+    for idx, show in enumerate(shows['shows']['items']):
+        if shows['shows']['items'][idx]['min_max']['min']<5 and shows['shows']['items'][idx]['min_max']['max']>6:
+            shows['shows']['items'][idx]['min_max']['min'] = 5
+            shows['shows']['items'][idx]['min_max']['max'] = int(round(shows['shows']['items'][idx]['min_max']['max'] + 5-(shows['shows']['items'][idx]['min_max']['max']%5), 0))
+    return shows
 
-for idx, show in enumerate(shows['shows']['items']):
-    if shows['shows']['items'][idx]['min_max']['min']<5 and shows['shows']['items'][idx]['min_max']['max']<6:
-        shows['shows']['items'][idx]['min_max']['min'] = 0
-        shows['shows']['items'][idx]['min_max']['max'] = 5
+shows = round_min_max()
 
-for idx, show in enumerate(shows['shows']['items']):
-    if shows['shows']['items'][idx]['min_max']['min']<5 and shows['shows']['items'][idx]['min_max']['max']>6:
-        shows['shows']['items'][idx]['min_max']['min'] = 5
-        shows['shows']['items'][idx]['min_max']['max'] = int(round(shows['shows']['items'][idx]['min_max']['max'] + 5-(shows['shows']['items'][idx]['min_max']['max']%5), 0))
-        
 #Create uniform duration spans
-for idx, show in enumerate(shows['shows']['items']):
-    if shows['shows']['items'][idx]['min_max']['min'] == 0 and shows['shows']['items'][idx]['min_max']['max'] == 5:
-        shows['shows']['items'][idx]['duration_span'] = 'under 5'
-    if 5 <= shows['shows']['items'][idx]['min_max']['min'] <= 15 and 5 <= shows['shows']['items'][idx]['min_max']['max'] <= 15:
-        shows['shows']['items'][idx]['duration_span'] = '5 to 15'
-    if 5 <= shows['shows']['items'][idx]['min_max']['min'] <= 15 and 15 <= shows['shows']['items'][idx]['min_max']['max'] <= 30:
-        shows['shows']['items'][idx]['duration_span'] = '15 to 30'
-    if 15 <= shows['shows']['items'][idx]['min_max']['min'] <= 30 and 15 <= shows['shows']['items'][idx]['min_max']['max'] <= 30:
-        shows['shows']['items'][idx]['duration_span'] = '15 to 30'
-    if 15 <= shows['shows']['items'][idx]['min_max']['min'] <= 30 and 30 <= shows['shows']['items'][idx]['min_max']['max'] <= 45:
-        shows['shows']['items'][idx]['duration_span'] = '30 to 45'
-    if 30 <= shows['shows']['items'][idx]['min_max']['min'] <= 45 and 30 <= shows['shows']['items'][idx]['min_max']['max'] <= 45:
-        shows['shows']['items'][idx]['duration_span'] = '30 to 45'
-    if 30 <= shows['shows']['items'][idx]['min_max']['min'] <= 45 and 45 <= shows['shows']['items'][idx]['min_max']['max']:
-        shows['shows']['items'][idx]['duration_span'] = 'over 45'
-    if 45 <= shows['shows']['items'][idx]['min_max']['min']:
-        shows['shows']['items'][idx]['duration_span'] = 'over 45'
+def get_uniform_duration_spans():
+    for idx, show in enumerate(shows['shows']['items']):
+        if shows['shows']['items'][idx]['min_max']['min'] == 0 and shows['shows']['items'][idx]['min_max']['max'] == 5:
+            shows['shows']['items'][idx]['duration_span'] = 'under 5'
+        if 5 <= shows['shows']['items'][idx]['min_max']['min'] <= 15 and 5 <= shows['shows']['items'][idx]['min_max']['max'] <= 15:
+            shows['shows']['items'][idx]['duration_span'] = '5 to 15'
+        if 5 <= shows['shows']['items'][idx]['min_max']['min'] <= 15 and 15 <= shows['shows']['items'][idx]['min_max']['max'] <= 30:
+            shows['shows']['items'][idx]['duration_span'] = '15 to 30'
+        if 15 <= shows['shows']['items'][idx]['min_max']['min'] <= 30 and 15 <= shows['shows']['items'][idx]['min_max']['max'] <= 30:
+            shows['shows']['items'][idx]['duration_span'] = '15 to 30'
+        if 15 <= shows['shows']['items'][idx]['min_max']['min'] <= 30 and 30 <= shows['shows']['items'][idx]['min_max']['max'] <= 45:
+            shows['shows']['items'][idx]['duration_span'] = '30 to 45'
+        if 30 <= shows['shows']['items'][idx]['min_max']['min'] <= 45 and 30 <= shows['shows']['items'][idx]['min_max']['max'] <= 45:
+            shows['shows']['items'][idx]['duration_span'] = '30 to 45'
+        if 30 <= shows['shows']['items'][idx]['min_max']['min'] <= 45 and 45 <= shows['shows']['items'][idx]['min_max']['max']:
+            shows['shows']['items'][idx]['duration_span'] = 'over 45'
+        if 45 <= shows['shows']['items'][idx]['min_max']['min']:
+            shows['shows']['items'][idx]['duration_span'] = 'over 45'
+    return shows
+
+shows = get_uniform_duration_spans()
 
 #Check that all every shows are assigned to a span
 for idx, show in enumerate(shows['shows']['items']):
     try:
         print(Fore.GREEN+ shows['shows']['items'][idx]['name'], shows['shows']['items'][idx]['duration_span'])
     except KeyError:
-        print(Fore.RED + 'span does not exist')
+        print(Fore.RED + 'span does not exist', idx)
         
-##Filter shows by duration
-for idx, show in enumerate(shows['shows']['items']):
-    if shows['shows']['items'][idx]['duration_span'] == '5 to 15':
-        print(Fore.CYAN + show['name'], Fore.MAGENTA + show['duration_span'])
+##Return shows which are matching input_duration
+ready_to_send = {}
+def return_shows(span):
+    for idx, show in enumerate(shows['shows']['items']):
+        try:
+            if shows['shows']['items'][idx]['duration_span'] == span:
+                print(Fore.CYAN + show['name'], Fore.MAGENTA + show['duration_span'])
+                ready_to_send[idx] = shows['shows']['items'][idx]
+                #Insert the command to send messages instead of print
+        except KeyError:
+            del shows['shows']['items'][idx]
+    return ready_to_send
+            
+ready_to_send = return_shows(span=input_duration)            
+
+if len(ready_to_send) >= 5:
+    print('sending shows')
+    #Insert the command to send ready_to_send[:4]
+if len(ready_to_send) < 5:
+    #Insert the command to send ready_to_send
+    shows = get_shows(key_words=input_key_words, language=input_language, input_offset=start_offset)
+    start_offset += 50
+    
+
 
 
 
