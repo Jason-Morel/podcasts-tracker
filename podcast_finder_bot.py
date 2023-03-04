d9bc2ea55ec306642fcf02c70f969bcce863ac58
lynabenyahia <122797987+lynabenyahia@users.noreply.github.com>
2023-03-04 15:30:22 +0100

    Update podcast_finder_bot.py

--- podcast_finder_bot.py
+++ podcast_finder_bot.py
@@ -17,80 +17,82 @@ from telegram import InlineKeyboardButton
 from telegram import InlineKeyboardMarkup
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
-  
-  
-############################### Bot ############################################
-def start(update, context):
-  update.message.reply_text(main_menu_message(),
-                            reply_markup=main_menu_keyboard())
 
-def main_menu(update,context):
-  query = update.callback_query
-  query.answer()
-  query.edit_message_text(
-                        text=main_menu_message(),
-                        reply_markup=main_menu_keyboard())
-  
-def first_menu(update,context):
-  query = update.callback_query
-  query.answer()
-  query.edit_message_text(
-                        text=first_menu_message(),
-                        reply_markup=first_menu_keyboard())
-
-def second_menu(update,context):
-  query = update.callback_query
-  query.answer()
-  query.edit_message_text(
-                        text=second_menu_message(),
-                        reply_markup=second_menu_keyboard())
-
-# and so on for every callback_data option
-def first_submenu(bot, update):
-  pass
-
-def second_submenu(bot, update):
-  pass
 
 ############################ Keyboards #########################################
 def main_menu_keyboard():
-  keyboard = [[InlineKeyboardButton('Option 1', callback_data='m1')],
-              [InlineKeyboardButton('Option 2', callback_data='m2')],
-              [InlineKeyboardButton('Option 3', callback_data='m3')]]
-  return InlineKeyboardMarkup(keyboard)
-
-def first_menu_keyboard():
-  keyboard = [[InlineKeyboardButton('Submenu 1-1', callback_data='m1_1')],
-              [InlineKeyboardButton('Submenu 1-2', callback_data='m1_2')],
-              [InlineKeyboardButton('Main menu', callback_data='main')]]
-  return InlineKeyboardMarkup(keyboard)
+    keyboard = [[InlineKeyboardButton('Liste de podcasts à écouter en une fois', callback_data='m1')],
+                [InlineKeyboardButton('Liste de show dont les épisodes seront proches de votre temps d\'écoute quotidien', callback_data='m2')]]
+    return InlineKeyboardMarkup(keyboard)
 
 def second_menu_keyboard():
-  keyboard = [[InlineKeyboardButton('Submenu 2-1', callback_data='m2_1')],
-              [InlineKeyboardButton('Submenu 2-2', callback_data='m2_2')],
-              [InlineKeyboardButton('Main menu', callback_data='main')]]
-  return InlineKeyboardMarkup(keyboard)
+    keyboard = [[InlineKeyboardButton('Moins de 5 minutes', callback_data='1')],
+                [InlineKeyboardButton('De 5 à 15 minutes', callback_data='2')],
+                [InlineKeyboardButton('De 15 à 30 minutes', callback_data='3')],
+                [InlineKeyboardButton('De 30 à 45 minutes', callback_data='4')],
+                [InlineKeyboardButton('Plus de 45 minutes', callback_data='5')]]
+    return InlineKeyboardMarkup(keyboard)
+
+def third_menu_keyboard():
+    keyboard = [[InlineKeyboardButton('Français', callback_data='fr')],
+                [InlineKeyboardButton('Anglais', callback_data='en')],
+                [InlineKeyboardButton('Espagnol', callback_data='es')],
+                [InlineKeyboardButton('Italien', callback_data='it')]
+                [InlineKeyboardButton('Allemand', callback_data='de')]]
+    return InlineKeyboardMarkup(keyboard)
 
 ############################# Messages #########################################
 def main_menu_message():
-  return 'Choose the option in main menu:'
-
-def first_menu_message():
-  return 'Choose the submenu in first menu:'
+    return 'Que préférez-vous ?'
 
 def second_menu_message():
-  return 'Choose the submenu in second menu:'
+    return 'Quel est le temps d\'écoute que vous souhaitez ?'
+
+def third_menu_message():
+    return 'Quelle langue souhaitez-vous pour vos podcasts ?'
+
+def fourth_menu_message():
+    return 'Quel type de podcasts souhaitez-vous écouter ?\nEntrez le thème de votre choix :'
 
 ############################# Handlers #########################################
+def start(update, context):
+    update.message.reply_text(main_menu_message(),
+                              reply_markup=main_menu_keyboard())
+
+def main_menu(update, context):
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text=second_menu_message(),
+                            reply_markup=second_menu_keyboard())
+
+def second_menu(update, context):
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text=third_menu_message(),
+                            reply_markup=third_menu_keyboard())
+
+def third_menu(update, context):
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text=fourth_menu_message())
+
+def fourth_menu(update, context):
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text="Vous avez choisi le thème : " + query.data)
+
+
 updater = Updater(BOT_TOKEN, use_context=True)
 
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CallbackQueryHandler(main_menu, pattern='main'))
-updater.dispatcher.add_handler(CallbackQueryHandler(first_menu, pattern='m1'))
-updater.dispatcher.add_handler(CallbackQueryHandler(second_menu, pattern='m2'))
-updater.dispatcher.add_handler(CallbackQueryHandler(first_submenu,
-                                                    pattern='m1_1'))
-updater.dispatcher.add_handler(CallbackQueryHandler(second_submenu,
-                                                    pattern='m2_1'))
+# Handlers
 
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CallbackQueryHandler(main_menu, pattern='m1'))
+updater.dispatcher.add_handler(CallbackQueryHandler(second_menu, pattern='m2'))
+updater.dispatcher.add_handler(CallbackQueryHandler(third_menu, pattern='m2_2'))
+updater.dispatcher.add_handler(CallbackQueryHandler(fourth_menu, pattern='m2_3'))
+
+# Start the bot
 updater.start_polling()
+updater.idle()
+
