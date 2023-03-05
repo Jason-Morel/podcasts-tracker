b423dca82f7dfe05d3c84e829fef8886197146da
Jason Morel <jason.morel3813@gmail.com>
2023-03-05 11:36:54 +0100

    Structuring the code

--- no_authentication.py
+++ no_authentication.py
@@ -13,24 +13,6 @@ import pandas as pd
 import re
 from colorama import Fore, Back, Style
 
-#Get client_id and client_secret from a .env file. 
-#We do this because it is unsafe to show one's API id/secret/token online as anyone could use them at our cost (if we pay to use the API).
-#SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
-#SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
-#Can't authenticate when id and secret are stored in .env file -> we need to figure out why.
-
-
-#client_credentials_manager = SpotifyClientCredentials(client_id=os.getenv('SPOTIPY_CLIENT_ID'), client_secret=os.getenv('SPOTIPY_CLIENT_SECRET'))
-#sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
-#Authentication - without user
-def authenticate():
-    sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id='e48f42372a074a25b7a0d25da48439d6',
-                                                               client_secret='90eb460ff94847998926f6d380532f59'))
-                                                               
-    return sp
-
-sp = authenticate()
-
 
 
 #User inputs
@@ -40,25 +22,30 @@ input_duration =  '15 to 30'
 
 
 
+#Get client_id and client_secret from a .env file. 
+#We do this because it is unsafe to show one's API id/secret/token online as anyone could use them at our cost (if we pay to use the API).
+#SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
+#SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
+#Can't authenticate when id and secret are stored in .env file -> we need to figure out why.
+
+
+
+#client_credentials_manager = SpotifyClientCredentials(client_id=os.getenv('SPOTIPY_CLIENT_ID'), client_secret=os.getenv('SPOTIPY_CLIENT_SECRET'))
+#sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+#Authentication - without user
+def authenticate():
+    sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id='e48f42372a074a25b7a0d25da48439d6',
+                                                               client_secret='90eb460ff94847998926f6d380532f59'))
+    return sp
+
+
+
 #Find shows related to economy in French market
 def get_shows(key_words, language, input_offset):
     shows = sp.search(q=key_words, limit=50, offset=input_offset, type='show', market=language)
     return shows
     #for idx, show in enumerate(shows['shows']['items']):
         #print(show['publisher'], ':', show['name'], show['languages'], show['id'])
-
-shows = get_shows(key_words=input_key_words, language=input_language, input_offset=start_offset)
-
-
-
-#Find the next 50 shows related to economy in French market (if none of the first 50 episodes matches the expected duration)
-shows_50 = sp.search(q='économie', limit=50, offset=50, type='show', market='FR')
-for idx, show in enumerate(shows_50['shows']['items']):
-    print(show['publisher'], ':', show['name'], show['languages'], show['id'])
-
-###TO DO: 
-    #create a loop to search for more shows if there isn't enough results with the first 50 shows.
-    #add these additional results on top of the first dictionary.
 
 
 
@@ -71,7 +58,7 @@ def remove_other_languages(language):
             del shows['shows']['items'][idx]
     return shows
 
-shows = remove_other_languages(language=input_language)
+
 
 #/!\ WARNING /!\
 #I have noticed that remove_other_languages doesn't work when input_key_word = 'donut'.
@@ -87,7 +74,7 @@ def get_episodes(language):
         shows['shows']['items'][idx]['episodes'] = sp.show_episodes(show_id=shows['shows']['items'][idx]['id'], limit=50, market=language)
     return shows
         
-shows = get_episodes(language=input_language)
+
 
 #2-Add a new key (called 'duration_ms') to the shows['shows']['items'][idx] dictionaries. It's a list which contains durations (in ms) of the show's first 50 episodes. 
 def get_durations():
@@ -98,7 +85,7 @@ def get_durations():
             shows['shows']['items'][idx]['durations_ms'] = duration_ms
     return shows
 
-shows = get_durations()
+
     
 #3-Find min and max values of the duration_ms list
 def get_min_max():
@@ -112,11 +99,7 @@ def get_min_max():
         shows['shows']['items'][idx]['min_max'] = {'min':round(int(durations_ms.quantile(q=0.1))/60000,2), 'max':round(int(durations_ms.quantile(q=0.9))/60000,2)} #min and max are converted to minutes
     return shows
 
-shows = get_min_max()
 
-#To have an idea of the intervals we can expect with this method:
-for idx, show in enumerate(shows['shows']['items']):
-    print(shows['shows']['items'][idx]['min_max'])
     
 #Drop shows for which episode duration is not regular enough
 def keep_shows_with_regular_duration():  
@@ -127,7 +110,7 @@ def keep_shows_with_regular_duration():
                 del shows['shows']['items'][idx]
     return shows
 
-shows = keep_shows_with_regular_duration()
+
 
 ##Assign a duration span to each show
 #Round min and max to get clean duration spans
@@ -148,7 +131,7 @@ def round_min_max():
             shows['shows']['items'][idx]['min_max']['max'] = int(round(shows['shows']['items'][idx]['min_max']['max'] + 5-(shows['shows']['items'][idx]['min_max']['max']%5), 0))
     return shows
 
-shows = round_min_max()
+
 
 #Create uniform duration spans
 def get_uniform_duration_spans():
@@ -171,14 +154,7 @@ def get_uniform_duration_spans():
             shows['shows']['items'][idx]['duration_span'] = 'over 45'
     return shows
 
-shows = get_uniform_duration_spans()
 
-#Check that all every shows are assigned to a span
-for idx, show in enumerate(shows['shows']['items']):
-    try:
-        print(Fore.GREEN+ shows['shows']['items'][idx]['name'], shows['shows']['items'][idx]['duration_span'])
-    except KeyError:
-        print(Fore.RED + 'span does not exist', idx)
         
 ##Return shows which are matching input_duration
 ready_to_send = {}
@@ -192,8 +168,21 @@ def return_shows(span):
         except KeyError:
             del shows['shows']['items'][idx]
     return ready_to_send
-            
-ready_to_send = return_shows(span=input_duration)            
+
+
+
+#To have an idea of the intervals we can expect with this method:
+for idx, show in enumerate(shows['shows']['items']):
+    print(shows['shows']['items'][idx]['min_max'])
+
+
+
+#Check that all every shows are assigned to a span
+for idx, show in enumerate(shows['shows']['items']):
+    try:
+        print(Fore.GREEN+ shows['shows']['items'][idx]['name'], shows['shows']['items'][idx]['duration_span'])
+    except KeyError:
+        print(Fore.RED + 'span does not exist', idx)
 
 
 
@@ -202,6 +191,7 @@ start_offset = 0
 ready_to_send = {}
 sent = 0
 
+sp = authenticate()
 
 
 while sent < 5:
