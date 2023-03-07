884fb49126c42d346d627c78f6d6acc0f2f2e38d
Jason Morel <jason.morel3813@gmail.com>
2023-03-07 19:03:07 +0100

    Cleaning_first_wave

--- MessageVersion.py
+++ MessageVersion.py
@@ -11,7 +11,7 @@ Created on Wed Feb 22 16:41:17 2023
 import spotipy # la librairie pour manipuler l'api spotify
 import time
 from spotipy.oauth2 import SpotifyClientCredentials
-from no_authentication import find_shows
+from show_treatment import find_shows
 from fonctions import find_episode
 from TelegramMessages import send_telegram_message, get_telegram_response
 
