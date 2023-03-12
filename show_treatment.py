d39c5abff0332e6ffd03f5e7323fbb4c93d932c2
Jason Morel <jason.morel3813@gmail.com>
2023-03-12 01:14:25 +0100

    Cleaning

--- show_treatment.py
+++ show_treatment.py
@@ -10,42 +10,37 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
 import re
 #from colorama import Fore, Back, Style
-import requests
+from TelegramMessages import send_telegram_message
 
 
 
-# # Infos de mon telegram :
-# TOKEN_telegram = "6179108053:AAFXqqyrlrLvN_tlSARu2_l3TLXkA_EjXTc" # obtenu en créant notre bot avec le telegram BotFather
-# chat_id = "6167298721" #obtenu en allant sur https://api.telegram.org/bot{TOKEN_telegram}/getUpdates
-
-
-
-# #User inputs
+###You might see some comments as the 4 lines below.
+# #User inputs:
 # input_key_words = 'économie'
 # input_duration =  '15 to 30'
 # time_choice = 2
+###These comments are pieces of code which have been useful during the development process.
+###They are kept as comments along this file to make debugging easier.
 
 
 
-#Get client_id and client_secret from a .env file. 
-#We do this because it is unsafe to show one's API id/secret/token online as anyone could use them at our cost (if we pay to use the API).
-#SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
-#SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
-#Can't authenticate when id and secret are stored in .env file -> we need to figure out why.
-
-
-
-#client_credentials_manager = SpotifyClientCredentials(client_id=os.getenv('SPOTIPY_CLIENT_ID'), client_secret=os.getenv('SPOTIPY_CLIENT_SECRET'))
-#sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
-#Authentication - without user
+#Authentication - without user.
+#This step is to 'log' in the Spotify API.
+#Remark: 
+    #It is not good practice to share API's credentials on GitHub.
+    #These keys should remain secret. Otherwise, anyone on GitHub could use the API with your account.
+    #You don't want this to happen. Especially if you registered your credit card details on the API...
+#Here we share our API's credentials because Spotify API is free to use. So we don't risk to be hacked.
+#Sharing our credentials makes our program easy to use for everyone.
 def authenticate():
     sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id='e48f42372a074a25b7a0d25da48439d6',
-                                                               client_secret='90eb460ff94847998926f6d380532f59'))
+                                                                             client_secret='90eb460ff94847998926f6d380532f59'))
     return sp
 
 
 
-#Find shows related to economy in French market
+#Find shows related to the input keyword.
+#French market is set by default.
 def get_shows(key_words, input_offset):
     shows = sp.search(q=key_words, limit=50, offset=input_offset, type='show', market='fr')
     return shows
@@ -54,7 +49,9 @@ def get_shows(key_words, input_offset):
 
 
 
-##Remove shows which are not matching selected language (LYNA: allow user to select languages)
+#Remove shows which are not matching selected language.
+#As of now, french is set by default.
+#But this function makes it easy to set language as an input.
 def remove_other_languages():
     for idx, show in enumerate(shows['shows']['items']):
         shows['shows']['items'][idx]['languages'] = str(shows['shows']['items'][idx]['languages']).lower()
@@ -68,12 +65,18 @@ def remove_other_languages():
 #/!\ WARNING /!\
 #I have noticed that remove_other_languages doesn't work when input_key_word = 'donut'.
 #I am guessing it has something to do with the fact that donut isn't a french word per se.
+#But the function re.search should still delete any show for which 'languages' is not matching 'fr'.
+#So the problem might come from this re.search actually.
 #To run an example of this bug, please see 'donut_issue.py' in this repo.
 
 
 
-##Look at the distribution of the variable duration_ms for the set of episodes
-#1-Add a new key (called 'episodes') to the shows['shows']['items'][idx] dictionaries. It's a dictionary which contains descriptions of the show's first 50 episodes.
+##The result object from function sp.search doesn't contain any information about shows' duration.
+##It makes sense because shows are composed by several episodes of different durations.
+##Therefore the idea behind the following lines is to approximate a shows 'duration' from its episodes durations.
+
+#1-Add a new key (called 'episodes') to the shows['shows']['items'][idx] dictionaries. 
+#It's a dictionary which contains descriptions of the show's first 50 episodes.
 def get_episodes():
     for idx, show in enumerate(shows['shows']['items']):
         shows['shows']['items'][idx]['episodes'] = sp.show_episodes(show_id=shows['shows']['items'][idx]['id'], limit=50, market='fr')
@@ -81,7 +84,8 @@ def get_episodes():
         
 
 
-#2-Add a new key (called 'duration_ms') to the shows['shows']['items'][idx] dictionaries. It's a list which contains durations (in ms) of the show's first 50 episodes. 
+#2-Add a new key (called 'duration_ms') to the shows['shows']['items'][idx] dictionaries. 
+#It's a list which contains durations (in ms) of the show's first 50 episodes. 
 def get_durations():
     for idx, show in enumerate(shows['shows']['items']):
         duration_ms = []
@@ -98,15 +102,25 @@ def get_min_max():
     for idx, show in enumerate(shows['shows']['items']):
         shows['shows']['items'][idx]['durations_ms'] = pd.DataFrame({'duration_ms':shows['shows']['items'][idx]['durations_ms']})
 
-    #3.2-Add a new key (called 'min_max') to the shows['shows']['items'][idx] dictionaries. It's a dictionary which contains the min (10th percentile) and max (90th percentile) of the show's first 50 episodes. 
+    #3.2-Add a new key (called 'min_max') to the shows['shows']['items'][idx] dictionaries. 
+    #It's a dictionary which contains the min (10th percentile) and max (90th percentile) of the show's first 50 episodes. 
     for idx, show in enumerate(shows['shows']['items']):
         durations_ms = shows['shows']['items'][idx]['durations_ms']
-        shows['shows']['items'][idx]['min_max'] = {'min':round(int(durations_ms.quantile(q=0.1))/60000,2), 'max':round(int(durations_ms.quantile(q=0.9))/60000,2)} #min and max are converted to minutes
+        #Convert min and max into minutes:
+        shows['shows']['items'][idx]['min_max'] = {'min':round(int(durations_ms.quantile(q=0.1))/60000,2), 
+                                                   'max':round(int(durations_ms.quantile(q=0.9))/60000,2)}
     return shows
+
+
+
+# #To have an idea of the intervals we can expect with this method:
+# for idx, show in enumerate(shows['shows']['items']):
+#     print(shows['shows']['items'][idx]['min_max'])
 
 
     
 #Drop shows for which episode duration is not regular enough
+#Shows for which max-min > 15 minutes will be droped.
 def keep_shows_with_regular_duration():  
     for idx, show in enumerate(shows['shows']['items']):
         if shows['shows']['items'][idx]['min_max']['min']<45:
@@ -118,12 +132,15 @@ def keep_shows_with_regular_duration():
 
 
 ##Assign a duration span to each show
-#Round min and max to get clean duration spans
+
+#1-Round min and max to get clean duration spans
 def round_min_max():
     for idx, show in enumerate(shows['shows']['items']):
         if shows['shows']['items'][idx]['min_max']['min']>5:
-            shows['shows']['items'][idx]['min_max']['min'] = int(round(shows['shows']['items'][idx]['min_max']['min'] - shows['shows']['items'][idx]['min_max']['min']%5, 0))
-            shows['shows']['items'][idx]['min_max']['max'] = int(round(shows['shows']['items'][idx]['min_max']['max'] - (shows['shows']['items'][idx]['min_max']['max']%5), 0))
+            shows['shows']['items'][idx]['min_max']['min'] = int(round(shows['shows']['items'][idx]['min_max']['min']
+                                                                       -shows['shows']['items'][idx]['min_max']['min']%5, 0))
+            shows['shows']['items'][idx]['min_max']['max'] = int(round(shows['shows']['items'][idx]['min_max']['max']
+                                                                       -shows['shows']['items'][idx]['min_max']['max']%5, 0))
     
     for idx, show in enumerate(shows['shows']['items']):
         if shows['shows']['items'][idx]['min_max']['min']<5 and shows['shows']['items'][idx]['min_max']['max']<6:
@@ -133,12 +150,14 @@ def round_min_max():
     for idx, show in enumerate(shows['shows']['items']):
         if shows['shows']['items'][idx]['min_max']['min']<5 and shows['shows']['items'][idx]['min_max']['max']>6:
             shows['shows']['items'][idx]['min_max']['min'] = 5
-            shows['shows']['items'][idx]['min_max']['max'] = int(round(shows['shows']['items'][idx]['min_max']['max'] + 5-(shows['shows']['items'][idx]['min_max']['max']%5), 0))
+            shows['shows']['items'][idx]['min_max']['max'] = int(round(shows['shows']['items'][idx]['min_max']['max']
+                                                                       +5
+                                                                       -shows['shows']['items'][idx]['min_max']['max']%5, 0))
     return shows
 
 
 
-#Create uniform duration spans
+#2-Create uniform duration spans
 def get_uniform_duration_spans():
     for idx, show in enumerate(shows['shows']['items']):
         if shows['shows']['items'][idx]['min_max']['min'] == 0 and shows['shows']['items'][idx]['min_max']['max'] == 5:
@@ -160,13 +179,16 @@ def get_uniform_duration_spans():
     return shows
 
 
-     
-def send_telegram_message(message, chat_id, TOKEN_telegram): 
-    url = f"https://api.telegram.org/bot{TOKEN_telegram}/sendMessage?chat_id={chat_id}&text={message}"
-    response = requests.get(url)
+
+# #Check that every shows are assigned to a span
+# for idx, show in enumerate(shows['shows']['items']):
+#     try:
+#         print(Fore.GREEN+ shows['shows']['items'][idx]['name'], shows['shows']['items'][idx]['duration_span'])
+#     except KeyError:
+#         print(Fore.RED + 'span does not exist', idx)
 
 
-
+#The following function has been created for the sake of compatibility between show_treatment and episode_treatment
 def range_for_shows(time_choice):
     if time_choice == 1:
         input_duration = 'under 5'
@@ -195,16 +217,18 @@ def return_shows(span, chat_id, TOKEN_telegram):
             if shows['shows']['items'][idx]['duration_span'] == span:
                 #print(Fore.CYAN + show['name'], Fore.MAGENTA + show['duration_span'])
                 send_telegram_message(f"{show['name']}\n{show['external_urls']['spotify']}", chat_id, TOKEN_telegram)
-                ready_to_send[idx] = shows['shows']['items'][idx]
-                #Insert the command to send messages instead of print
+                ready_to_send[idx] = shows['shows']['items'][idx] #This dictionary will allow us to count how many results have been sent.
         except KeyError:
-            del shows['shows']['items'][idx]
+            del shows['shows']['items'][idx] 
     return ready_to_send
+
+#except KeyError has been added below in case a show is not assigned to any span.
+#It will delete the 'lost' show and the loop will keep going instead of getting a KeyError.
 
 
 
 def find_shows(input_key_words, time_choice, chat_id, TOKEN_telegram):
-    #Algo input
+    
     start_offset = 0
     sent = 0
 
@@ -229,35 +253,11 @@ def find_shows(input_key_words, time_choice, chat_id, TOKEN_telegram):
         ready_to_send = return_shows(input_duration, chat_id, TOKEN_telegram)
         sent += len(ready_to_send)
 
-
-
-# #To have an idea of the intervals we can expect with this method:
-# for idx, show in enumerate(shows['shows']['items']):
-#     print(shows['shows']['items'][idx]['min_max'])
-
-
-
-# #Check that all every shows are assigned to a span
-# for idx, show in enumerate(shows['shows']['items']):
-#     try:
-#         print(Fore.GREEN+ shows['shows']['items'][idx]['name'], shows['shows']['items'][idx]['duration_span'])
-#     except KeyError:
-#         print(Fore.RED + 'span does not exist', idx)
-
+#Function sp.search gives at most 50 results. It means we have to launch another search if we want more than 50 results.
+#That's what the start_offset is for. It allows us to get the next 50 results instead of getting the same results as the first search.
 
     
-    
 
-#Notes:
-
-#If there isn't any result with a pair (subject; duration), we could ask if user wants to see results for other durations (if there is any).
-#OR the user could be asked only a subject and algo could send results which are grouped by duration.      
-
-
-#Because people usually commute twice a day (home -> office AND office -> home): we could return episodes matching the duration selected by user AND episodes which are twice as long.
-#It would allow the user to listen half of the episode in the morning and the other half in the evening.
-
-#How can we approximate episodes duration if API limits the results to 50?
-#We could look at the distribution of episodes duration for a specific show.
-#Then we would recommend shows to user (instead of episodes).
-#It means our tool would not need to be used everyday. Instead, user could use our tool when they feel like discovering new shows which match a specific topic.
+#Some ways to improve our tool (non-exhaustive):
+    #If there isn't any result with a pair (keyword; duration), we could ask if user wants to see results for other durations (if there are any).
+    #OR the user could be asked only a subject and algo could send results which are grouped by duration.      
